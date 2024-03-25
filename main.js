58b1eb7cbde12294dc1831d5b538ed6824fb685a
rickydang123 <ricky.dang@torontomu.ca>
2024-03-25 04:15:14 -0400

    lab 8

--- main.js
+++ main.js
@@ -14,3 +14,38 @@ function enlargeImg() {
     img.style.transform = "scale(1.35)";
     img.style.transition = "transform 0.25s ease";
 }
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+
+    // Change button image based on mode
+    const button = document.querySelector('.toggle-btn');
+    const currentMode = body.classList.contains('dark-mode') ? 'image/lightmode.png' : 'image/darkmode.png';
+    button.innerHTML = `<img src="${currentMode}" alt="Light Bulb" width="84" height="48">`;
+  }
+
+// Get the button
+    var mybutton = document.getElementById("scrollBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function scrollToTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
