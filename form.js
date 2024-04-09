9de65d47056816d0400722cf92926d8a1e6239f1
rickydang123 <ricky.dang@torontomu.ca>
2024-04-08 21:03:03 -0400

    Final

--- form.js
+++ form.js
@@ -2,22 +2,22 @@ function storeData(event) {
     event.preventDefault(); // prevent the form from submitting normally
 
     const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
     const email = document.getElementById("email").value;
-    const country = document.getElementById("country").value;
     const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
 
-    if (fname === "" || lname === "" || email === "" || country === "" || subject === "") {
+    if (fname === "" || email === "" || subject === "" || message === "") {
         alert("Please fill out all required fields.");
         return false;
       }
 
     // Store form data in local storage
     localStorage.setItem("fname", fname);
-    localStorage.setItem("lname", lname);
     localStorage.setItem("email",email);
-    localStorage.setItem("country", country);
     localStorage.setItem("subject", subject);
+    localStorage.setItem("message", message);
 
     alert("Submission Successful")
     }
+
+    
